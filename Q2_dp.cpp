4385c69df74adb388b97ae54f3eb45a6568afe58
Suhaila419 <163913554+Suhaila419@users.noreply.github.com>
2024-12-27 22:06:46 +0000

    Add files via upload

---
+++ Q2_dp.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+   ll MOD=1000000007;
+   string s;cin>>s;
+   vector<ll>dp(s.size()+1);
+   if(s.find("m")==-1&&s.find("w")==-1){
+        dp[0]=1;
+        dp[1]=1;
+     for(int i=2;i<=s.size();i++){
+        dp[i]=dp[i-1];
+        if((s[i-2]=='n'&&s[i-1]=='n')||(s[i-2]=='u'&&s[i-1]=='u')){
+            dp[i]=(dp[i-1]+dp[i-2])%MOD;
+
+        }
+     }
+     cout<<dp[s.size()]<<"\n";
+   }
+   else cout<<0<<"\n";
+    return 0;
+}
