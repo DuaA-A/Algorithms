97299474bb507c0f6d4ffeea9f957814845f9323
Suhaila419 <163913554+Suhaila419@users.noreply.github.com>
2024-12-30 18:48:08 +0000

    Update Q5.cpp

--- Assignment3/MST/Q5.cpp
+++ Assignment3/MST/Q5.cpp
@@ -1,146 +1,130 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
+#include <vector>
 #include <algorithm>
+#include <string>
+#include <cctype>
+#include <deque>
+#include <queue>
+#include <cstring>
 #include <set>
-#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#include <list>
+#include <map>
+#include <random>
+#include <unordered_map>
+
 using namespace std;
 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<double> vd;
+typedef vector<long long> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<vll> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<vpi> vvpi;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
 
-string addBitStrings( string first, string second );
+const long long mod = 1000000007;
 
-int makeEqualLength(string &str1, string &str2){
-    int len1 = str1.size();
-    int len2 = str2.size();
-    if (len1 < len2){
-        for (int i = 0 ; i < len2 - len1 ; i++)
-            str1 = '0' + str1;
-        return len2;
-    }
-    else if (len1 > len2){
-        for (int i = 0 ; i < len1 - len2 ; i++)
-            str2 = '0' + str2;
-    }
-    return len1;
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(c) (c).begin(),(c).end()
+#define sz(c) static_cast<int>((c).size())
+#define forn(i, a, b) for(int i = a; i < b; i++)
+
+#define pb push_back
+#define mp make_pair
+
+const int MAXN = 100000;
+vector<int> lst[MAXN];
+int parent[MAXN];
+
+void make_set(int v) {
+    lst[v] = vector<int>(1, v);
+    parent[v] = v;
 }
 
+int find_set(int v) {
+    return parent[v];
+}
 
-string addBitStrings( string first, string second ){
-    string result;
-    int length = makeEqualLength(first, second);
-    int carry = 0;
-    for (int i = length-1 ; i >= 0 ; i--){
-        int firstBit = first.at(i) - '0';
-        int secondBit = second.at(i) - '0';
-        int sum = (firstBit ^ secondBit ^ carry)+'0';
-        result = (char)sum + result;
-        carry = (firstBit & secondBit) | (secondBit & carry) | (firstBit & carry);
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (lst[a].size() < lst[b].size())
+            swap(a, b);
+        while (!lst[b].empty()) {
+            int v = lst[b].back();
+            lst[b].pop_back();
+            parent[v] = a;
+            lst[a].push_back(v);
+        }
     }
-    if (carry)
-        result = '1' + result;
-    return result;
+}
+
+vvpi nb;
+vi ans(200100, 0);
+vi used;
+int n, m;
+
+int dfs(int v) {
+    used[v] = 1;
+    int ssz = 1;
+    for (auto u : nb[v]) {
+        if (used[u.first]) continue;
+
+        ll st = dfs(u.first);
+        ssz += st;
+
+        ll r = st * (n - st);
+        int cur = u.second;
+        while (r > 0) {
+            if (r % 2) ans[cur]++;
+            r /= 2;
+            if (ans[cur] == 2) {
+                ans[cur] = 0;
+                r++;
+            }
+            cur++;
+        }
+    }
+    return ssz;
 }
 
 int main() {
     FAST;
-	int V, E;
-	cin>>V>>E;
-	int **matrix = new int*[V];
-	for(int i = 0; i < V; i++) {
-		matrix[i] = new int[V];
-		for(int j = 0; j < V; j++) {
-			matrix[i][j] = -1;
-		}
-	}
-	pair<int,int> *edges = new pair<int,int>[E];
-	for(int i = 0; i < E; i++) {
-		int v1, v2, w;
-		cin>>v1>>v2>>w;
-		matrix[v1-1][v2-1] = w;
-		matrix[v2-1][v1-1] = w;
-		edges[w] = pair<int,int>(v1-1,v2-1);
-	}
-	string answer = "0";
-	for(int i = 0; i < V; i++) {
-		set<int> sortedEdges;
-		int *visited = new int[V];
-		for(int i = 0; i < V; i++) {
-			visited[i] = false;
-		}
-		visited[i] = true;
-		for(int j = 0; j < V; j++) {
-			if(matrix[i][j] != -1) {
-				sortedEdges.insert(matrix[i][j]);
-			}
-		}
-		bool visitedAllVertices = true;
-		for(int i = 0; i < V; i++) {
-			if(!visited[i]) {
-				visitedAllVertices = false;
-				break;
-			}
-		}
-		string *dist = new string[E];
-		for(int i = 0; i < E; i++) {
-			dist[i] = string(E, '0');
-		}
-		while(!visitedAllVertices) {
-			auto it = sortedEdges.begin();
-			int v1 = edges[*it].first;
-			int v2 = edges[*it].second;
-			bool edgeRemoved = false;
-			if(!visited[v2]) {
-				for(int j = 0; j < dist[v1].size(); j++) {
-					if(dist[v1][j] == '1') {
-						dist[v2][j] = '1';
-					}
-				}
-				dist[v2][E-*it-1] = '1';
-				if(v2 > i) {
-					answer = addBitStrings(answer, dist[v2]);
-				}
-				visited[v2] = true;
-				sortedEdges.erase(it);
-				edgeRemoved = true;
+    vector<pair<int, pi>> e;
+    cin >> n >> m;
+    forn(i, 0, m) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        e.pb(mp(c, mp(a - 1, b - 1)));
+    }
+    nb.resize(n);
+    forn(i, 0, n) make_set(i);
+    sort(all(e));
 
-				for(int j = 0; j < V; j++) {
-					if(matrix[v2][j] != -1) {
-						sortedEdges.insert(matrix[v2][j]);
-					}
-				}
-			} else if(!visited[v1]) {
-				for(int j = 0; j < dist[v2].size(); j++) {
-					if(dist[v2][j] == '1') {
-						dist[v1][j] = '1';
-					}
-				}
-				dist[v1][E-*it-1] = '1';
-				if(v1 > i) {
-					answer = addBitStrings(answer, dist[v1]);
-				}
-				visited[v1] = true;
-				sortedEdges.erase(it);
-				edgeRemoved = true;
-
-				for(int j = 0; j < V; j++) {
-					if(matrix[v1][j] != -1) {
-						sortedEdges.insert(matrix[v1][j]);
-					}
-				}
-			}
-			if(!edgeRemoved) {
-				sortedEdges.erase(it);
-			}
-			visitedAllVertices = true;
-			for(int i = 0; i < V; i++) {
-				if(!visited[i]) {
-					visitedAllVertices = false;
-					break;
-				}
-			}
-		}
-	}
-	cout<<answer;
+    for (auto u : e) {
+        int a = u.second.first;
+        int b = u.second.second;
+        int x = find_set(a);
+        int y = find_set(b);
+        if (x == y) continue;
+        int c = u.first;
+        nb[a].pb(mp(b, c));
+        nb[b].pb(mp(a, c));
+        union_sets(a, b);
+    }
+    used = vi(n, 0);
+    dfs(0);
+    while (ans.back() == 0) ans.pop_back();
+    reverse(all(ans));
+    for (auto x : ans) cout << x;
+    cout << "\n";
     return 0;
 }
